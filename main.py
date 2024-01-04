2e66877be3e6563b7cee20275903328af2055514
Chanwoo <tree698@gmail.com>
2024-01-04 14:29:40 +0900

    save news to local + refactoring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, dotenv, requests
 from datetime import datetime, timedelta
 from crawling import WebCrawling
-from web_address import ko_sources
+from web_address import ko_sources, ko_selector, en_sources, en_selector
 
 dotenv.load_dotenv()
 sheety_endpoint_ko = os.getenv('SHEETY_ENDPOINT_KO')
@@ -9,8 +9,8 @@ sheety_endpoint_en = os.getenv('SHEETY_ENDPOINT_EN')
 bearer_header = {
     "Authorization": f"Bearer {os.getenv('BEARER_HEADER')}"
 }
-
 body = {'news': {}}
+
 today = datetime.now()
 today_formatted = today.strftime("%Y/%m/%d")
 yesterday = today - timedelta(days=1)
@@ -19,49 +19,56 @@ week_today = today.weekday()
 week_yesterday = yesterday.weekday()
 
 
+def save(date, name_, news):
+    if len(news) == 0:
+        print(f'{date} | {name_} is empty.')
+    news_list = news.split('&&&')
+    for news in news_list:
+        with open('news.txt', 'a') as content:
+            content.write(f'{date} | {name_} | {news}\n')
+
+
+def sheety(endpoint, body_, headers):
+    response_ = requests.post(endpoint, json=body_, headers=headers)
+    response_.raise_for_status()
+    return response_
+
+
+def reset_body(source):
+    for key, url in source.items():
+        del body['news'][key]
+
+
 # TODO 1. Korean Newspapers
 for key, url in ko_sources.items():
     crawled = WebCrawling(url)
-    crawled_news = crawled.news('._persist_wrap > div:nth-child(1) > div:nth-child(1) .newspaper_brick_item._start_page li')
+    crawled_news = crawled.news(ko_selector['selector'])
     body['news'][key] = crawled_news
+    save(today_formatted, key, crawled_news)
 
 body['news']['date'] = today_formatted
 body['news']['week'] = week_today
-sheety_response = requests.post(sheety_endpoint_ko, json=body, headers=bearer_header)
-sheety_response.raise_for_status()
-
+sheety(sheety_endpoint_ko, body, bearer_header)
 print(body)
-for key, url in ko_sources.items():
-    del body['news'][key]
+reset_body(ko_sources)
 
 
-# TODO 2. Other Newspapers
-names = ['nyt', 'wp', 'ft']
+# TODO 2. English Newspapers
+for key, url in en_sources.items():
+    crawled = WebCrawling(url)
+    crawled_news = crawled.news(en_selector[key])
+    body['news'][key] = crawled_news
+    save(yesterday_formatted, key, crawled_news)
 
-# The New York Times (nyt)
-crawled =WebCrawling('https://www.nytimes.com/section/todayspaper?redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Finternational%2F')
-crawled_nyt = crawled.news('.css-1u3p7j1')
-body['news'][names[0]] = crawled_nyt
-
-# The Washington Post (wp)
-crawled =WebCrawling('https://www.washingtonpost.com/todays_paper/updates/')
-crawled_wp = crawled.news('#Front-Page .wpds-c-eGurKC')
-body['news'][names[1]] = crawled_wp
-
-# The Finanical Times (ft)
-crawled =WebCrawling('https://www.ft.com/')
-crawled_ft = crawled.news('#top-stories + .layout-desktop__grid-container .text.text--color-black.text-display--scale-3.text--weight-500')
-body['news'][names[2]] = crawled_ft
-
-# sheety
 body['news']['date'] = yesterday_formatted
 body['news']['week'] = week_yesterday
-sheety_response = requests.post(sheety_endpoint_en, json=body, headers=bearer_header)
-sheety_response.raise_for_status()
-
+sheety(sheety_endpoint_en, body, bearer_header)
 print(body)
-for name in names:
-    del body['news'][name]
+reset_body(en_sources)
+
+
+
+
 
 
 
--- web_address.py
+++ web_address.py
@@ -10,5 +10,20 @@ ko_sources = {
     '매경': 'https://media.naver.com/press/009/newspaper',
 }
 
+ko_selector = {
+    'selector': '._persist_wrap > div:nth-child(1) > div:nth-child(1) .newspaper_brick_item._start_page li'
+}
+
+en_sources = {
+    'nyt': 'https://www.nytimes.com/section/todayspaper?redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Finternational%2F',
+    'wp': 'https://www.washingtonpost.com/todays_paper/updates/',
+    'ft': 'https://www.ft.com/'
+}
+
+en_selector = {
+    'nyt': '.css-1u3p7j1',
+    'wp': '#Front-Page .wpds-c-eGurKC',
+    'ft': '#top-stories + .layout-desktop__grid-container .text.text--color-black.text-display--scale-3.text--weight-500'
+}
 
 
