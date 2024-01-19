8363a60064414197935c7eabbff075b1b51da8ca
Chanwoo <tree698@gmail.com>
2024-01-19 18:12:02 +0900

    modify headers for Japanese newspaper

--- crawling.py
+++ crawling.py
@@ -23,16 +23,13 @@ class WebCrawling:
     https://stackoverflow.com/questions/57983718/could-not-scrape-a-japanese-website-using-beautifulsoup
     """
     def jp_news(self, css_selector):
-        response = requests.session()
         headers = {
             'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
-            # 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
             'User-Agent': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6',
         }
-        response.headers.update(headers)
 
         try:
-            contents = response.get(self.url)
+            contents = requests.get(self.url, headers=headers)
             contents.raise_for_status()
         except HTTPError:
             title_list = []
